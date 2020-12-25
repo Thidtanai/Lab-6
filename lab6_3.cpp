8087f61ae26b78932515b46f75003f938bef9541
Thidtanai <thidtanai_k@cmu.ac.th>
2020-12-25 16:57:50 +0700

    Update lab6_3

--- lab6_3.cpp
+++ lab6_3.cpp
@@ -2,7 +2,18 @@
 using namespace std;
 
 char before(char x){
-	//Write your function definition here
+	if(x > 'Z'){
+		return '0';
+	}
+	else if(x == 'A'){
+		return 'Z';
+	}
+	else if(x < 'A'){
+		return '0';
+	}
+	else{
+		return x-1;
+	}
 }
 
 int main(){
